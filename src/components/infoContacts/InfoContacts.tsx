47813d5be508f46a3da8dcdd30d9149381a6f0ba
lopatop <pavellopato327@gmail.com>
2024-12-05 18:15:43 +0100

    components InfoContacts

---
+++ src/components/infoContacts/InfoContacts.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Icon} from "../icon/Icon";
+import styled from "styled-components";
+
+export const InfoContacts = () => {
+    return (
+        <ul>
+            <li>
+                <a href=""><Icon iconId={'github'} width={'25'} height={'25'}/></a>
+            </li>
+            <li>
+                <a href=""><Icon iconId={'github'} width={'25'} height={'25'}/></a>
+            </li>
+            <li>
+                <a href=""><Icon iconId={'github'} width={'25'} height={'25'}/></a>
+            </li>
+        </ul>
+    );
+};
+
