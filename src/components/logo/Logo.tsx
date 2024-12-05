1defdf66ea8fa4adb116ecdf8247321a81e0c7eb
lopatop <pavellopato327@gmail.com>
2024-12-05 18:15:00 +0100

    components Logo

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Icon} from "../icon/Icon";
+
+
+
+export const Logo = () => {
+    return (
+        <a href=''>
+            <Icon iconId ={'github'} height={'25'} width={'25'}/>
+        </a>
+    );
+};
+
