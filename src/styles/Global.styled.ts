ddc2ef88d7929b3f523cf6e25194d3a08bc043fb
lopatop <pavellopato327@gmail.com>
2024-12-11 21:05:17 +0100

    Global styled

---
+++ src/styles/Global.styled.ts
@@ -0,0 +1,45 @@
+import {createGlobalStyle} from 'styled-components';
+import {theme} from "./Theme";
+
+export const GlobalStyled = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        box-sizing: border-box;
+        background-color: ${theme.colors.primaryBg};
+        
+    }
+    ol, ul {
+        list-style: none;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    button {
+        background-color: unset;
+        border: none;
+    }
+
+    input,
+    textarea,
+    select {
+        background: none;
+        border: none;
+        outline: none;
+    }
+    
+    body {
+        margin: 0;
+        font-family: 'Poppins', 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    
+`
