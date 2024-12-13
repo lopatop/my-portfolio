7e3b9f3d458d8e47a393e6c40b6779d6228c9050
lopatop <pavellopato327@gmail.com>
2024-12-13 21:25:18 +0100

    Styled InfoContacts Main

--- src/components/infoContacts/InfoContacts.tsx
+++ src/components/infoContacts/InfoContacts.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import {Icon} from "../icon/Icon";
+import styled from "styled-components";
 
 export const InfoContacts = () => {
     return (
-        <ul>
+        <StyledInfoContacts>
             <li>
-                <a href=""><Icon iconId={'github'} width={'25'} height={'25'}/></a>
+                <a href=""><Icon iconId={'instagram'} width={'30'} height={'30'} viewBox={'0 0 45 45'}/></a>
             </li>
             <li>
-                <a href=""><Icon iconId={'github'} width={'25'} height={'25'}/></a>
+                <a href=""><Icon iconId={'telegram'} width={'30'} height={'30'} viewBox={'0 0 32 32'}/></a>
             </li>
             <li>
-                <a href=""><Icon iconId={'github'} width={'25'} height={'25'}/></a>
+                <a href=""><Icon iconId={'githubMenu'} width={'30'} height={'30'} viewBox={'0 0 95 95'}/></a>
             </li>
-        </ul>
+        </StyledInfoContacts>
     );
 };
 
+const StyledInfoContacts = styled.ul`
+    display: flex;
+    gap: 20px;
+    
+`
--- src/layout/sections/works/works/workLink/link/Link.tsx
+++ src/layout/sections/works/works/workLink/link/Link.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../../../../../components/icon/Icon";
+import {theme} from "../../../../../../styles/Theme";
 
 type LinkPropsType = {
     iconId: string;
     href?: string;
     text:string;
+    viewBox?:string
 
 }
 
 export const Link = (props:LinkPropsType) => {
     return (
         <StyledLink>
-            <Icon iconId={props.iconId} width={"25"} height={"25"}></Icon>
+            <Icon iconId={props.iconId} width={"25"} height={"25"} viewBox={props.viewBox}></Icon>
             <Links href={props.href}>{props.text}</Links>
         </StyledLink>
     );
@@ -23,5 +25,10 @@ display: flex;
     gap: 3px
 `
 const Links = styled.a`
-
+    font-weight: 400;
+    font-size: 16px;
+    text-decoration: underline;
+    text-decoration-skip-ink: none;
+    color: ${theme.colors.titleFont};
+    
 `
