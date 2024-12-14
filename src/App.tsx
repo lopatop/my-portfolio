ccbf89c34d06f04e412728c975570d37f944ba30
lopatop <pavellopato327@gmail.com>
2024-12-14 08:10:20 +0100

    app components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import './App.css';
-import styled from 'styled-components';
-import {Header} from "./loyaut/header/Header";
-import {Main} from "./loyaut/sections/main/Main";
-import {TechStack} from "./loyaut/sections/techStack/TechStack";
+import {Header} from "./layout/header/Header";
+import {Main} from "./layout/sections/main/Main";
+import {TechStack} from "./layout/sections/techStack/TechStack";
+import {Works} from "./layout/sections/works/Works";
+import {Contact} from "./layout/sections/contact/Contact";
+import {Footer} from "./layout/footer/Footer";
+import {About} from "./layout/sections/about/About";
+import "./fonts.css";
+
 
 
 function App() {
     return (
         <div className="App">
+
             <Header/>
             <Main/>
+            <About/>
             <TechStack/>
+            <Works/>
+            <Contact/>
+            <Footer/>
+
         </div>
     );
 }
 
 export default App;
-
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
