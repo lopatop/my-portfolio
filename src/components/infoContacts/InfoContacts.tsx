6ba148e5faca86c69986d201cb585eebb517fa6e
lopatop <pavellopato327@gmail.com>
2024-12-18 21:00:57 +0100

    add Links InfoContacts

--- src/components/infoContacts/InfoContacts.tsx
+++ src/components/infoContacts/InfoContacts.tsx
@@ -6,13 +6,15 @@ export const InfoContacts = () => {
     return (
         <StyledInfoContacts>
             <li>
-                <a href=""><Icon iconId={'instagram'} width={'30'} height={'30'} viewBox={'0 0 45 45'}/></a>
+                <a href="https://www.instagram.com/lopatop/" target="_blank">
+                    <Icon iconId={'instagram'} width={'30'} height={'30'} viewBox={'0 0 45 45'}/>
+                </a>
             </li>
             <li>
-                <a href=""><Icon iconId={'telegram'} width={'30'} height={'30'} viewBox={'0 0 32 32'}/></a>
+                <a href="https://t.me/lopatop" target="_blank"><Icon iconId={'telegram'} width={'30'} height={'30'} viewBox={'0 0 32 32'}/></a>
             </li>
             <li>
-                <a href=""><Icon iconId={'githubMenu'} width={'30'} height={'30'} viewBox={'0 0 95 95'}/></a>
+                <a href="https://github.com/lopatop" target="_blank"><Icon iconId={'githubMenu'} width={'30'} height={'30'} viewBox={'0 0 95 95'}/></a>
             </li>
         </StyledInfoContacts>
     );
@@ -28,6 +30,4 @@ const StyledInfoContacts = styled.ul`
             transform: translateY(-4px);
         }
     }
-   
-    
 `
