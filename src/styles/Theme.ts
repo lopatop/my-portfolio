0458bd040cf9193321cab85e4f9866176e59f8a8
lopatop <pavellopato327@gmail.com>
2024-12-19 21:08:54 +0100

    media desktop tablet mobile

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -2,11 +2,18 @@
 
 export const theme = {
     colors: {
-        primaryBg: "#191919",
+        // "#191919",
+        primaryBg: "#FFFFFF",
         menuFont: "#a7a7a7",
         titleFont:"#d9d9d9",
         textFont:"#ccc",
-    }
+        projectFont: "#363636",
+    },
 
+    media:{
+        desktop:"screen and (max-width: 1024px)",
+        tablet: "screen and (max-width: 768px)",
+        mobile: "screen and (max-width: 576px)",
+    }
 
 }
