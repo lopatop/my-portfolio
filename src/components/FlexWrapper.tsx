3605524bbdf5cb63cf7148bff3f236f922fdc295
lopatop <pavellopato327@gmail.com>
2024-12-19 21:10:11 +0100

    add props gap to FlexWrapperPropsType

--- src/components/FlexWrapper.tsx
+++ src/components/FlexWrapper.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 type FlexWrapperPropsType = {
-    direction?: string;
-    justify?: string;
-    align?: string;
+    direction?: string
+    justify?: string
+    align?: string
     wrap?: string
+    gap?:string
 
 }
 
@@ -14,5 +15,7 @@ export const FlexWrapper = styled.div<FlexWrapperPropsType>`
     justify-content: ${props => props.justify || "flex-start"};
     align-items: ${props => props.align || "stretch"};
     flex-wrap: ${props => props.wrap || "nowrap"};
+    gap: ${props => props.gap || "0"};
+    
     
 `
