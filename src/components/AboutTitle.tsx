c8920e2af0b235b5d76eda9f5dc84ff98ac6bff7
lopatop <pavellopato327@gmail.com>
2024-12-22 19:13:23 +0100

    add font AboutTitle

--- src/components/AboutTitle.tsx
+++ src/components/AboutTitle.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 import {theme} from "../styles/Theme";
+import {font} from "../styles/Common";
 
 export const AboutTitle = styled.h3`
-    font-weight: 700;
-    font-size: 42px;
+    ${font({ weight: 700, Fmin: 28, Fmax: 42 })};
     letter-spacing: -0.01em;
     color: ${theme.colors.textFont};
 `
