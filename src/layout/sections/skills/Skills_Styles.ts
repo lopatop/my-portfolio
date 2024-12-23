af7c96bfe6ad2eca45e1f24d3ed600edacc82594
lopatop <pavellopato327@gmail.com>
2024-12-23 21:12:48 +0100

    Skills_Styles

---
+++ src/layout/sections/skills/Skills_Styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import {theme} from "../../../styles/Theme";
+
+
+const Skills = styled.section`
+    padding-top: 145px;
+    
+    @media ${theme.media.mobile} {
+        padding-top: 20px;
+    }
+    
+    
+`
+
+const Skill = styled.div`
+    width: 25%;
+    height: 170px;
+    padding-bottom: 50px;
+    display: flex;
+    justify-content: space-around;
+    @media ${theme.media.mobile} {
+        width: 50%;
+    }
+    
+`
+
+export const S = {
+    Skills,
+    Skill,
+
+}
