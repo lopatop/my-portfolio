cffa4ff6b9cc1f368616fd716fb4f69d06ff984d
lopatop <pavellopato327@gmail.com>
2024-12-23 21:11:08 +0100

    Header_Styles

---
+++ src/layout/header/Header_Styles.ts
@@ -0,0 +1,38 @@
+
+import styled from "styled-components";
+import {Container} from "../../components/Container";
+import {theme} from "../../styles/Theme";
+
+
+
+const Header = styled.header`
+    padding-top: 40px;
+    position: sticky;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 99999;
+    ${Container}{
+        background-color: rgba(31, 31, 32, 0.9);
+    }
+`
+const InfoWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 55px;
+    font-family: "DM Sans", sans-serif;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 1.3;
+    text-align: center;
+    @media ${theme.media.desktop} {
+        display: none ;
+    }
+ 
+    
+`
+
+export const S = {
+    Header,
+    InfoWrapper,
+}
