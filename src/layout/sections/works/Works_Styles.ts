26d470ab7dd802a4f9f79b8557f5a11707582d60
lopatop <pavellopato327@gmail.com>
2024-12-23 21:13:03 +0100

    Works_Styles

---
+++ src/layout/sections/works/Works_Styles.ts
@@ -0,0 +1,138 @@
+
+
+
+
+import styled from "styled-components";
+import {theme} from "../../../styles/Theme";
+import {font} from "../../../styles/Common";
+
+
+//Works
+
+const Works = styled.section`
+    
+
+    @media ${theme.media.mobile} {
+        padding-top: 20px;
+    }
+
+`
+//Work
+
+
+const Work = styled.div`
+    background: ${theme.colors.projectFont};
+    border-radius: 20px;
+    max-width: 375px;
+    width: 100%;
+    
+`
+const Image = styled.img`
+    width: 100%;
+    height: 260px;
+    object-fit: cover;
+    border-radius: 20px 20px 0 0;
+    
+`
+const CardContainer = styled.div`
+    padding: 28px 28px 25px;
+    
+    @media ${theme.media.mobile}{
+        padding: 18px 18px 16px;
+    } 
+    
+`
+
+const Title = styled.h3`
+    ${font({ weight: 500, Fmin:20, Fmax: 28 })};
+
+    line-height: 0.92857;
+    color: ${theme.colors.textFont};
+
+`
+const Text = styled.p`
+    ${font({ weight: 300, Fmin:14, Fmax: 18 })};
+
+    line-height: 1.44444;
+    padding-top: 17px;
+    color: ${theme.colors.textFont};
+    
+    @media ${theme.media.mobile} {
+        padding-top: 14px;
+    }
+    
+`
+const Paragraph = styled.p`
+    padding-top: 12px;
+
+    @media ${theme.media.mobile} {
+        padding-top: 8px;
+    }
+
+`
+
+
+const TextStack = styled.span`
+    ${font({ weight: 400, Fmin:12, Fmax: 16 })};
+    line-height: 1.625;
+    color: ${theme.colors.textFont};
+
+
+`
+
+const Technologies = styled.span`
+    ${font({ weight: 300, Fmin:10, Fmax: 14 })};
+    line-height: 1.625;
+    color: ${theme.colors.textFont};
+`
+
+//Link
+
+const Link = styled.div`
+display: flex;
+    gap: 3px
+`
+const Links = styled.a`
+    ${font({weight:400,Fmax:16, Fmin:14})}
+    text-decoration: underline;
+    text-decoration-skip-ink: none;
+    color: ${theme.colors.titleFont};
+
+    transition: transform 0.3s ease;
+    :hover{
+        transform: translateY(-4px);
+        
+    }
+    
+`
+
+//WorkLink
+
+const WorkLink = styled.div`
+display: flex;
+    gap: 48px;
+    padding-top: 20px;
+    
+    @media ${theme.media.mobile} {
+        gap: 30px;
+        padding-top: 10px;
+    }
+    
+`
+
+
+export const S = {
+    Works,
+    Work,
+    Image,
+    CardContainer,
+    Title,
+    Text,
+    Paragraph,
+    TextStack,
+    Technologies,
+    Link,
+    Links,
+    WorkLink,
+
+}
