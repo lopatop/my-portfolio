0f001e8cfe6c38b3cb469c8279e96a5b4907e59a
lopatop <pavellopato327@gmail.com>
2024-12-25 19:23:01 +0100

    Add Particle

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import './App.css';
 import {Header} from "./layout/header/Header";
 import {Main} from "./layout/sections/main/Main";
-import {TechStack} from "./layout/sections/techStack/TechStack";
+import {Skills} from "./layout/sections/skills/Skills";
 import {Works} from "./layout/sections/works/Works";
 import {Contact} from "./layout/sections/contact/Contact";
 import {Footer} from "./layout/footer/Footer";
 import {About} from "./layout/sections/about/About";
 import "./fonts.css";
-
+import React from "react";
+import {Particle} from "./components/particle/Particle";
 
 
 function App() {
     return (
         <div className="App">
-
+            <Particle/>
             <Header/>
             <Main/>
             <About/>
-            <TechStack/>
+            <Skills/>
             <Works/>
             <Contact/>
             <Footer/>
-
         </div>
     );
 }
