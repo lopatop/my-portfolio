c1555a8ec4c7bec703e7e26bd042f60eddafcbc9
lopatop <pavellopato327@gmail.com>
2024-12-26 19:52:55 +0100

    change Menu

--- src/layout/header/headerMenu/menu/Menu.tsx
+++ src/layout/header/headerMenu/menu/Menu.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import {S} from '../HeaderMenu_Styles'
 
-export const Menu: React.FC<{menuItems: Array<string>}> = (props: {menuItems: Array<string>}) => {
+type MenuItem = {
+    title: string;
+    href: string;
+};
+
+type MenuProps = {
+    menuItems: MenuItem[];
+    onMenuItemClick?: () => void; // Новый проп
+};
+
+export const Menu: React.FC<MenuProps> = ({menuItems, onMenuItemClick}) => {
     return (
-        <ul>
-            {props.menuItems.map((items, index,) => {
+        <ul >
+            {menuItems.map((items, index,) => {
                     return (
-                        <S.MenuItem key={index}>
-                            <S.LinkItem href={`#${items.toLowerCase()}`}>{items}</S.LinkItem>
+                        <S.MenuItem key={index} >
+                            <S.LinkItem
+                                to={items.href}
+                                smooth={true}
+                                spy={true}
+                                offset={-50}
+                                onClick={onMenuItemClick}
+                            >
+                                {items.title}
+                            </S.LinkItem>
                         </S.MenuItem>
                     )
                 }
