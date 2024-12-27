0d770dc37d83cf2372dbe5532262dc78236dba9b
lopatop <pavellopato327@gmail.com>
2024-12-27 08:23:57 +0100

    add new colars Theme

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -2,16 +2,19 @@
 
 export const theme = {
     colors: {
-        // "#191919",
-        primaryBg: "#FFFFFF",
+        primaryBg: "#191919",
         menuFont: "#a7a7a7",
         titleFont:"#d9d9d9",
         textFont:"#ccc",
         projectFont: "#363636",
+        borderBlock: "#4a4a4a",
+        fieldBg: "#252527",
+        fieldBorder: "rgba(220, 86, 236, 0.82)",
+        placeholder: "#495057"
     },
 
     media:{
-        desktop:"screen and (max-width: 1024px)",
+        desktop:"screen and (max-width: 1100px)",
         tablet: "screen and (max-width: 768px)",
         mobile: "screen and (max-width: 576px)",
     }
