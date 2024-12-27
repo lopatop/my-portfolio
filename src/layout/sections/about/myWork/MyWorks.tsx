c6fed98cda6f96e2ee0e30c3b2d298fff1dcfbc8
lopatop <pavellopato327@gmail.com>
2024-12-27 08:23:30 +0100

    MyWorks MyWorksData

--- src/layout/sections/about/myWork/MyWorks.tsx
+++ src/layout/sections/about/myWork/MyWorks.tsx
@@ -1,37 +1,49 @@
 import React from 'react';
-import styled from "styled-components";
 import {AboutTitle} from "../../../../components/AboutTitle";
-import {Experience} from "../experience";
 import {FlexWrapper} from "../../../../components/FlexWrapper";
+import { S } from '../About_Styles';
+import {Experience} from "../experience/Experience";
 
+const MyWorksData = [
+    {
+        name: "Junior Web Developer",
+        time: "Sep 2021 - Dec 2021",
+        place: "Bengaluru",
+        company: "Dr.Rajkumars Learning App",
+        deadline: "Full Time",
+    },
+    {
+        name: "Junior Web Developer",
+        time: "Sep 2021 - Dec 2021",
+        place: "Bengaluru",
+        company: "Dr.Rajkumars Learning App",
+        deadline: "Full Time",
+    },
+    {
+        name: "Junior Web Developer",
+        time: "Sep 2021 - Dec 2021",
+        place: "Bengaluru",
+        company: "Dr.Rajkumars Learning App",
+        deadline: "Full Time",
+    },
+]
 
-
-export const MyWorks = () => {
+export const MyWorks: React.FC = () => {
     return (
-        <StyledMyWorks>
+        <S.MyWorks>
             <FlexWrapper direction="column" justify="space-between" gap="30px">
-            <AboutTitle>Work Experience</AboutTitle>
-                <Experience name={"Junior Web Developer"}
-                            time={"Sep 2021 - Dec 2021"}
-                            place={"Bengaluru"}
-                            company={"Dr.Rajkumars Learning App"}
-                            deadline={"Full Time" }/>
-                <Experience name={"Junior Web Developer"}
-                            time={"Sep 2021 - Dec 2021"}
-                            place={"Bengaluru"}
-                            company={"Dr.Rajkumars Learning App"}
-                            deadline={"Full Time" }/>
-                <Experience name={"Junior Web Developer"}
-                            time={"Sep 2021 - Dec 2021"}
-                            place={"Bengaluru"}
-                            company={"Dr.Rajkumars Learning App"}
-                            deadline={"Full Time" }/>
+                <AboutTitle>Work Experience</AboutTitle>
+                {MyWorksData.map((w, index) => {
+                    return <Experience name={w.name}
+                                       time={w.time}
+                                       place={w.place}
+                                       company={w.company}
+                                       deadline={w.deadline}
+                                       key={index}/>
 
+                })}
             </FlexWrapper>
-        </StyledMyWorks>
+        </S.MyWorks>
     );
 };
 
-const StyledMyWorks = styled.div`
-
-`
